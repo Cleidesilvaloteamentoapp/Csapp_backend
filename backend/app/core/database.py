cf4520093d4ee4c59584a187eea2fe930f29021c
onovotrafego <nacs.promoter@gmail.com>
2026-05-19 12:45:26 -0300

    fix: deletar cliente

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -10,7 +10,7 @@ from app.core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    echo=settings.DEBUG,
+    echo=False,
     pool_size=20,
     max_overflow=10,
     pool_pre_ping=True,
